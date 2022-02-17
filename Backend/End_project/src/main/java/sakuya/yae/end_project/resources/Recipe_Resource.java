de25216f2865faba5f851191ff63b68fb61a7a2e
SakuyaYae <darkreimu@outlook.com>
2022-02-17 13:45:26 +0100

    Update Recipe_Resource.java

--- Backend/End_project/src/main/java/sakuya/yae/end_project/resources/Recipe_Resource.java
+++ Backend/End_project/src/main/java/sakuya/yae/end_project/resources/Recipe_Resource.java
@@ -5,10 +5,30 @@
  */
 package sakuya.yae.end_project.resources;
 
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import sakuya.yae.end_project.beans.Recipe_Bean;
+import sakuya.yae.end_project.entities.Recipe;
+
 /**
  *
  * @author SakuyaYae
  */
 public class Recipe_Resource {
-    
+    @EJB
+    Recipe_Bean recipe_bean;
+    @POST
+    public Response postManga(Recipe recipe){
+        if(recipe_bean.postRecipe(recipe)){
+            return Response.status(Response.Status.CREATED).build();
+        }
+        else{
+            return Response.serverError().entity("Oops somting went worng!").build();
+        }
+    }
 }

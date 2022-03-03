268cf9600c3620d3950d5c1161e38677ebea6361
SakuyaYae <darkreimu@outlook.com>
2022-03-03 13:43:06 +0100

    added things

---
+++ Backend/End_project/src/main/java/sakuya/yae/end_project/beans/User_bean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sakuya.yae.end_project.beans;
+
+import sakuya.yae.end_project.entities.Users;
+
+/**
+ *
+ * @author SakuyaYae
+ */
+public class User_bean {
+    public void createUser(String user_data){
+    
+    }
+    
+    public boolean checkUser(Users user){
+        return true;
+    }
+    
+    public void saveUser(Users user){
+    
+    }
+}

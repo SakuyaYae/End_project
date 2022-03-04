722c9a7129e5a5c143e780baf724e254abed3be6
SakuyaYae <89073955+SakuyaYae@users.noreply.github.com>
2022-03-04 12:07:53 +0100

    Add files via upload

--- Backend/End_project/src/main/java/sakuya/yae/end_project/beans/User_bean.java
+++ Backend/End_project/src/main/java/sakuya/yae/end_project/beans/User_bean.java
@@ -5,22 +5,63 @@
  */
 package sakuya.yae.end_project.beans;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Base64;
+import javax.ejb.Stateless;
+import sakuya.yae.end_project.Connection_Factory;
 import sakuya.yae.end_project.entities.Users;
 
 /**
  *
  * @author SakuyaYae
  */
+@Stateless
 public class User_bean {
-    public void createUser(String user_data){
-    
+    public Users createUser(String user_data){
+        user_data = user_data.substring(6).trim();
+        
+        byte bytes[] = Base64.getDecoder().decode(user_data);
+        user_data = new String(bytes);
+        int data_separetor = user_data.indexOf(":");
+        String username = user_data.substring(0, data_separetor);
+        String password = user_data.substring(data_separetor + 1);
+        return new Users(username, password);
     }
     
     public boolean checkUser(Users user){
-        return true;
+          try(Connection con = Connection_Factory.getconnection()){
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM users WHERE username = ?");
+            stmt.setString(1, user.getUser());
+            ResultSet res = stmt.executeQuery();
+            if(res.next()){
+                String bcryped_hash_str = res.getString("pass");
+                BCrypt.Result data = BCrypt.verifyer().verify(user.getPassword().toCharArray(), bcryped_hash_str);
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch(Exception e){
+            System.out.println("failed to veryfi user" + e.getMessage());
+            return false;
+        }
     }
     
-    public void saveUser(Users user){
-    
+    public int saveUser(Users user){
+        try(Connection con = Connection_Factory.getconnection()){
+            String Hashed_password = BCrypt.withDefaults().hashToString(12, user.getPassword().toCharArray());
+            PreparedStatement stmt = con.prepareStatement("INSERT INTO users(username, pass) VALUES(?, ?)");
+            stmt.setString(1, user.getUser());
+            stmt.setString(2, user.getPassword());
+            return stmt.executeUpdate();
+        }
+        catch(Exception e){
+            System.out.println("error in saveing user" + e.getMessage());
+            return 0;
+        }
     }
 }
---
+++ Backend/End_project/src/main/java/sakuya/yae/end_project/resources/User_Resource.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sakuya.yae.end_project.resources;
+
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import sakuya.yae.end_project.beans.User_bean;
+import sakuya.yae.end_project.entities.Users;
+
+/**
+ *
+ * @author Sakuya
+ */
+@Path("user")
+public class User_Resource {
+    @EJB
+    User_bean user_bean;
+    
+    @GET
+    public Response check_user(@HeaderParam("Authorization") String authorization){
+        Users user = user_bean.createUser(authorization);
+        if(user_bean.checkUser(user)){
+            return Response.status(Response.Status.CREATED).build();
+        }
+        else{
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+    @GET
+    public Response create_user(@HeaderParam("Authorization") String authorization){
+        Users user = user_bean.createUser(authorization);
+        if(user_bean.saveUser(user) == 1){
+            return Response.status(Response.Status.CREATED).build();
+        }
+        else{
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+}

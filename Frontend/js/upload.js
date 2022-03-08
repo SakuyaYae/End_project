ea29e22461547d3b76ba7442a92f28d5d8d753d5
SakuyaYae <89073955+SakuyaYae@users.noreply.github.com>
2022-03-08 14:42:35 +0100

    added functionalety

--- Frontend/js/upload.js
+++ Frontend/js/upload.js
@@ -1,5 +1,33 @@
-window.onload = main()
-
 function main(){
+  var upload = document.getElementById("upload");
+  upload.onclick = post_recipe();
   console.info("Sakura")
 }
+
+function post_recipe(){
+  var recipe_input = user_input();
+  send_data(recipe_input);
+  console.info("====")
+}
+
+function user_input(){
+  var title = document.getElementById("title");
+  var description = document.getElementById("description");
+  var ingrediens = document.getElementById("ingrediens");
+  var categories = document.getElementById("categories");
+  var step_by_setp_guide = document.getElementById("step_by_setp_guide");
+  var img = document.getElementById("img");
+  var recipe_data = [title, description, ingrediens, categories, step_by_setp_guide, img];
+
+  for(var i = 0; i < recipe_data.length; i++){
+    recipe_data[i] = String(recipe_data[i].value)
+  }
+
+  return recipe_data;
+}
+
+function send_data(recipe){
+  console.info("Sending")
+}
+
+window.onload = main()
